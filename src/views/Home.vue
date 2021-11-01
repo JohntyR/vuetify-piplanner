eb7c2daa14bc1ed24831d5012a42055b53015c03
John <jronis@gmail.com>
2021-11-01 19:58:56 +1000

    Update main sprint grid area
    add sprint grid
    add feature button placeholder

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,27 @@
 <template>
-  <hello-world />
+<div>
+  <SprintGrid />
+  <v-btn
+      class="mx-2"
+      fab
+      dark
+      color="indigo"
+    >
+      <v-icon dark>
+        mdi-plus
+      </v-icon>
+    </v-btn>
+</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  import SprintGrid from '../components/SprintGrid.vue'
 
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
+      SprintGrid,
     },
   }
 </script>
